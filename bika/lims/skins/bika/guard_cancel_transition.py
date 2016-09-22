b061449fabcc4f2928b48f42c59778ab2b1bcf58
Campbell <campbell@bikalabs.com>
2016-09-22 11:30:23 +0200

    ALS: Allow cancellation of ARs containing advanced-stage analyses

--- bika/lims/skins/bika/guard_cancel_transition.py
+++ bika/lims/skins/bika/guard_cancel_transition.py
@@ -17,19 +17,4 @@ from bika.lims.permissions import CancelAndReinstate
 
 checkPermission = context.portal_membership.checkPermission
 
-if context.portal_type == 'AnalysisRequest':
-    for analysis in context.getAnalyses(full_objects = True):
-        if not checkPermission(CancelAndReinstate, analysis):
-            return False
-    return True
-
-elif context.portal_type == 'Sample':
-    for ar in context.getAnalysisRequests():
-        if not checkPermission(CancelAndReinstate, ar):
-            return False
-        for analysis in ar.getAnalyses(full_objects = True):
-            if not checkPermission(CancelAndReinstate, analysis):
-                return False
-    return True
-
 return True
