0c46d85fc68f992f83d99be1d3efaea5c563191e
Campbell <campbell@bikalabs.com>
2017-01-16 14:37:21 +0200

    LIMS-2496: COA publication handles multi_ar email publication better
    This prevents sending multi_ar reports once for each AR in the report!

--- bika/lims/browser/js/bika.lims.analysisrequest.publish.js
+++ bika/lims/browser/js/bika.lims.analysisrequest.publish.js
@@ -29,7 +29,7 @@ function AnalysisRequestPublishView() {
         // Report is hidden by default in css
         load_barcodes();
         load_layout();
-        window.bika.lims.RangeGraph.load();
+//        window.bika.lims.RangeGraph.load();
         convert_svgs();
 
         // Store referrer in cookie in case it is lost due to a page reload
