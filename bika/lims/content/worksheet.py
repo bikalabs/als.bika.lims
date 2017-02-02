senaite/senaite.core

commit 4b62446053566f48d1d1917fffce7f852d5274ba

Added multi¡verification to referenceanalises and worksheets too...
Conflicts:
	bika/lims/browser/bika_listing.py
	bika/lims/content/analysisrequest.py
	bika/lims/content/worksheet.py

bika/lims/content/worksheet.py
@@ -752,12 +752,13 @@ class Worksheet(BaseFolder, HistoryAwareMixin):
                     success = True
                     revers = analysis.getNumberOfRequiredVerifications()
                     nmvers = analysis.getNumberOfVerifications()
-                    analysis.setNumberOfVerifications(nmvers+1)
+                    username=getToolByName(context,'portal_membership').getAuthenticatedMember().getUserName()
+                    item.addVerificator(username)
                     if revers-nmvers <= 1:
                         success, message = doActionFor(analysis, 'verify')
                         if not success:
-                            # If failed, restore to the previous number
-                            analysis.setNumberOfVerifications(nmvers)
+                            # If failed, delete last verificator.
+                            item.deleteLastVerificator()
                 else:
                     doActionFor(analysis, 'verify')
 

bika/lims/browser/bika_listing.py
@@ -217,12 +217,12 @@ class WorkflowAction:
                         success = True
                         revers = item.getNumberOfRequiredVerifications()
                         nmvers = item.getNumberOfVerifications()
-                        username=getToolByName(self.context,'portal_membership').getAuthenticatedMember().getUserName()
+                        username=getToolByName(context,'portal_membership').getAuthenticatedMember().getUserName()
                         item.addVerificator(username)
                         if revers-nmvers <= 1:
                             success, message = doActionFor(item, action)
                             if not success:
-                                # If failed, remove the last verification
+                                # If failed, delete last verificator.
                                 item.deleteLastVerificator()
                     else:
                         success, message = doActionFor(item, action)

bika/lims/content/analysis.py
@@ -1092,7 +1092,7 @@ class Analysis(BaseContent):
             return False
 
         #Checking verifiability depending on multi-verification type of bika_setup
-        if self.bika_setup.getNumberOfRequiredVerifications>1:
+        if self.bika_setup.getNumberOfRequiredVerifications()>1:
             mv_type=self.bika_setup.getTypeOfmultiVerification()
             #If user verified before and self_multi_disabled, then return False
             if mv_type=='self_multi_disabled' and self.wasVerifiedByUser(username):

bika/lims/content/analysisrequest.py
@@ -2989,12 +2989,13 @@ class AnalysisRequest(BaseFolder):
                     success = True
                     revers = analysis.getNumberOfRequiredVerifications()
                     nmvers = analysis.getNumberOfVerifications()
-                    analysis.setNumberOfVerifications(nmvers+1)
+                    username=getToolByName(context,'portal_membership').getAuthenticatedMember().getUserName()
+                    item.addVerificator(username)
                     if revers-nmvers <= 1:
                         success, message = doActionFor(analysis, 'verify')
                         if not success:
-                            # If failed, restore to the previous number
-                            analysis.setNumberOfVerifications(numvers)
+                            # If failed, delete last verificator
+                            item.deleteLastVerificator()
                 else:
                     doActionFor(analysis, 'verify')
 

bika/lims/content/referenceanalysis.py
@@ -137,12 +137,10 @@ schema = BikaSchema.copy() + Schema((
     # NumberOfRequiredVerifications from the Analysis Service
     IntegerField('NumberOfRequiredVerifications', default=1),
 
-    # Number of verifications done for this analysis. Each time a 'verify'
-    # transition takes place, this value is updated accordingly. The
-    # transition will finally succeed when the NumberOfVerifications matches
-    # with the NumberOfRequiredVerifications. Meanwhile, the state of the
-    # object will remain in 'to_be_verified'
-    IntegerField('NumberOfVerifications', default=0),
+    # This field keeps the user_ids of members who verified this analysis.
+    # After each verification, user_id will be added end of this string
+    # seperated by comma- ',' .
+    StringField('Verificators',default='')
 ),
 )
 
@@ -158,6 +156,31 @@ class ReferenceAnalysis(BaseContent):
     def _renameAfterCreation(self, check_auto_id=False):
         from bika.lims.idserver import renameAfterCreation
         renameAfterCreation(self)
+
+    def getNumberOfVerifications(self):
+        verificators=self.getVerificators()
+        if not verificators:
+            return 0
+        return len(verificators.split(','))
+
+    def addVerificator(self,username):
+        verificators=self.getVerificators()
+        if not verificators:
+            self.setVerificators(username)
+        else:
+            self.setVerificators(verificators+","+username)
+
+    def deleteLastVerificator(self):
+        verificators=self.getVerificators().split(',')
+        del verificators[-1]
+        self.setVerificators(",".join(verificators))
+
+    def wasVerifiedByUser(self,username):
+        verificators=self.getVerificators().split(',')
+        return username in verificators
+
+    def getLastVerificator(self):
+        return self.getVerificators().split(',')[-1]
 
     def Title(self):
         """ Return the Service ID as title """
@@ -462,6 +485,21 @@ class ReferenceAnalysis(BaseContent):
         selfverification = self.getService().isSelfVerificationEnabled()
         if self_submitted and not selfverification:
             return False
+
+        #Checking verifiability depending on multi-verification type of bika_setup
+        if self.bika_setup.getNumberOfRequiredVerifications()>1:
+            mv_type=self.bika_setup.getTypeOfmultiVerification()
+            #If user verified before and self_multi_disabled, then return False
+            if mv_type=='self_multi_disabled' and self.wasVerifiedByUser(username):
+                return False
+
+            # If user is the last verificator and consecutively multi-verification
+            # is disabled, then return False
+            # Comparing was added just to check if this method is called before/after
+            # verification
+            elif mv_type=='self_multi_not_cons' and username==self.getLastVerificator() and \
+                self.getNumberOfVerifications()<self.getNumberOfRequiredVerifications():
+                return False
 
         # All checks pass
         return True

bikalabs/als.bika.lims

commit 516633db05cd1c9386bed69ce6f1b3fd469cfa63

Fix error introduced in 4b62446053566f48d1d1917fffce7f852d5274ba

bika/lims/content/worksheet.py
@@ -836,12 +836,12 @@ class Worksheet(BaseFolder, HistoryAwareMixin):
                     revers = analysis.getNumberOfRequiredVerifications()
                     nmvers = analysis.getNumberOfVerifications()
                     username=getToolByName(self,'portal_membership').getAuthenticatedMember().getUserName()
-                    item.addVerificator(username)
+                    analysis.addVerificator(username)
                     if revers-nmvers <= 1:
                         success, message = doActionFor(analysis, 'verify')
                         if not success:
                             # If failed, delete last verificator.
-                            item.deleteLastVerificator()
+                            analysis.deleteLastVerificator()
                 else:
                     doActionFor(analysis, 'verify')
 