b469aa6b68b0b063303f9a4d005ad8f4a4edd714
veroc <oliver.roch@d9t.de>
2017-11-16 18:14:31 +0100

    do not use localized date for chart js. (#2306)
    * do not use localized date for chart js.
    
    * return empty string on missing capture date.

--- bika/lims/browser/referencesample.py
+++ bika/lims/browser/referencesample.py
@@ -159,7 +159,8 @@ class ReferenceAnalysesView(AnalysesView):
         service = obj.getService()
         item['Category'] = service.getCategoryTitle()
         item['Service'] = service.Title()
-        item['Captured'] = self.ulocalized_time(obj.getResultCaptureDate())
+        capture_date = obj.getResultCaptureDate()
+        item['Captured'] = self.ulocalized_time(capture_date)
         brefs = obj.getBackReferences("WorksheetAnalysis")
         item['Worksheet'] = brefs and brefs[0].Title() or ''
         # The following item keywords are required for the
@@ -167,6 +168,7 @@ class ReferenceAnalysesView(AnalysesView):
         # they are not actually used in the table rendering.
         item['Keyword'] = service.getKeyword()
         item['Unit'] = service.getUnit()
+        item['CapturedRaw'] = capture_date and capture_date.strftime('%Y-%m-%d %I:%M %p') or ''
 
         self.addToJSON(obj, service, item)
         return item
@@ -194,7 +196,7 @@ class ReferenceAnalysesView(AnalysesView):
             upper = smax + error_amount
             lower = smin - error_amount
 
-            anrow = {'date': item['Captured'],
+            anrow = {'date': item['CapturedRaw'],
                      'min': smin,
                      'max': smax,
                      'target': target,
